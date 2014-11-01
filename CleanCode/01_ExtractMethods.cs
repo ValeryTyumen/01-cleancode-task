51e5730590a6b511e67d972ef1313d8981b3489d
Валерий <v.lihosherstov@gmail.com>
2014-11-01 18:19:21 +0500

    changes

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -5,26 +5,38 @@ namespace CleanCode
 {
 	public static class RefactorMethod
 	{
-		private static void SaveData(string s, byte[] d)
+		private static void SaveData(string line, byte[] data)
 		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
+			var fileStream1 = new FileStream(line, FileMode.OpenOrCreate);
+		    string changeExtension = Path.ChangeExtension(line, "bkp");
+		    var fileStream2 = new FileStream(changeExtension, FileMode.OpenOrCreate);
 
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
+			WriteData(data, fileStream1, fileStream2);
 
-			// close files
-			fs1.Close();
-			fs2.Close();
+		    CloseFiles(fileStream1, fileStream2);
 
-			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+			SaveLastWriteTime(line);
 		}
+
+	    private static void SaveLastWriteTime(string line)
+	    {
+	        string path = line + ".time";
+	        var fileStream3 = new FileStream(path, FileMode.OpenOrCreate);
+	        var time = BitConverter.GetBytes(DateTime.Now.Ticks);
+	        fileStream3.Write(time, 0, time.Length);
+	        fileStream3.Close();
+	    }
+
+	    private static void CloseFiles(FileStream fileStream1, FileStream fileStream2)
+	    {
+	        fileStream1.Close();
+	        fileStream2.Close();
+	    }
+
+	    private static void WriteData(byte[] data, FileStream fileStream1, FileStream fileStream2)
+	    {
+	        fileStream1.Write(data, 0, data.Length);
+	        fileStream2.Write(data, 0, data.Length);
+	    }
 	}
 }
